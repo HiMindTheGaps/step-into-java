89490421e34cc5a4a9c4fe8f4c64b12d0b81053b
lihanwen <lihanwen@fenbi.com>
2020-05-31 09:49:30 +0800

    MOD: init commit package class

---
+++ src/main/java/com/lhw/study/netty/InitCommitClass.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)InitCommitClass.java, 5月 31, 2020.
+ * <p>
+ * Copyright 2020 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.lhw.study.netty;
+
+/**
+ * @author lihanwen
+ */
+public class InitCommitClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello hw");
+    }
+}
---
+++ src/main/java/com/xsq/study/stepintojava/InitCommitClassXsq.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)InitCommitClassXsq.java, 5月 31, 2020.
+ * <p>
+ * Copyright 2020 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.xsq.study.stepintojava;
+
+/**
+ * @author lihanwen
+ */
+public class InitCommitClassXsq {
+
+    public static void main(String[] args) {
+        System.out.println("hello xsq");
+    }
+}
